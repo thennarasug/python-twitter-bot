c8e5a69172950979d020cbf8c98a02233adfed46
s0uthk1ng <51071207+s0uthk1ng@users.noreply.github.com>
2021-02-24 19:22:17 +0900

    Update tbot.py
    fine tuned

--- tbot.py
+++ tbot.py
@@ -9,6 +9,10 @@ from textblob import TextBlob
 # Twython twitter api
 from twython import Twython, TwythonError
 
+import time
+import traceback
+import sqlite3
+
 '''
 #no more command line or dynamic arguments, just kept it for reference.
 
@@ -16,8 +20,39 @@ keywords = input("twitter search keyword? ")
 rt_fav_limit = input("max retweet_count or favorite_count? ")
 '''
 
-keywords_rt_fav_limits_all = [[50, ['signalapp', 'degoogle', 'f-droid', 'pinebook', 'pine64', 'pinephone']], [50, ['algotrading']], [100, ['tutanota', 'protonmail', 'duckduckgo', 'protonvpn', 'tails OS', 'tor browser', 'manjarolinux', 'manjaro']], [150, ['linux', 'ubuntu']]]
+def create_table():
+    c = db.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS pollachi (id string primary key)")
+    try:
+        db.commit()
+    except:
+        db.rollback()
 
+
+def insert_error_string(id):
+    c = db.cursor()
+    try:
+        vals = [id]
+        query = "INSERT INTO pollachi (id) VALUES (?)"
+        c.execute(query, vals)
+        db.commit()
+    except sqlite3.IntegrityError as i:
+        pass
+    except:
+        error = traceback.print_exc()
+        db.rollback()
+
+
+def select_error_string(id):
+    try:
+        query = "select count(*) from pollachi where id = '{}'".format(id)
+        cursor = db.execute(query)
+        db.commit()
+        return cursor
+    except:
+        error = traceback.print_exc()
+        db.rollback()
+        return None
 
 
 # TelegramBot method
@@ -47,6 +82,15 @@ def analysis(sentence):
     # print("Sentiment:\n{}\n\n".format(sentiment))
     return sentiment.polarity
 
+
+keywords_rt_fav_limits_all = [[50, ['signalapp', 'degoogle', 'f-droid', 'pinebook', 'pine64', 'pinephone']], [50, ['algotrading']], [100, ['tutanota', 'protonmail', 'duckduckgo', 'protonvpn', 'tails OS', 'tor browser', 'manjarolinux', 'manjaro']], [150, ['linux', 'ubuntu']]]
+
+
+# for database
+dbfilewithpath = "./tbot.db"
+db = sqlite3.connect(dbfilewithpath)
+
+create_table()
 
 APP_KEY = environ.get('APP_KEY')
 APP_SECRET = environ.get('APP_SECRET')
@@ -104,39 +148,43 @@ while True:
                         search_results = twitter.search(q=keyword, count=100, lang=lang_list, result_type=result_type)
                         # print (search_results)
                     except TwythonError as e:
-                        print(e)
+                        print("error on search ", e)
 
                     if len(search_results) > 0:
                         count = 0
                         for tweet in search_results['statuses']:
                             if "RT @" not in tweet['text'] and (tweet['favorite_count'] >= int(rt_fav_limit) or tweet['retweet_count'] >= int(rt_fav_limit)) and tweet['retweeted'] == False and tweet['is_quote_status'] == False:  # and tweet['possibly_sensitive'] == False:
                                 polarity_result = analysis(tweet['text'])
-                                if polarity_result >= 0.2 or lang_list == 'ta':
+                                if polarity_result >= 0.15 or lang_list == 'ta':
                                     try:
-                                        if len(tweet['text']) > 200 or "https" in tweet['text']:
-                                            twitter.retweet(id=int(tweet['id']))
-                                            print(tweet['text'])
-                                            sendtotelegram(TELEGRAM_TOKEN, TELEGRAM_TELEGRAM_CHAT_ID, tweet['text'])
-                                            time.sleep(5)
-                                        else:
-                                            tempStatus = "RT @" + tweet['user']['screen_name'] + " : " + tweet['text'] + " via #5k6mbot"
-                                            # this code is commented because of difficult in identifying the polls. #TODO
-                                            # twitter.update_status(status=tempStatus.encode('utf-8'))
-                                            twitter.retweet(id=int(tweet['id']))
-                                            sendtotelegram(TELEGRAM_TOKEN, TELEGRAM_TELEGRAM_CHAT_ID, tweet['text'])
-                                            print(tempStatus.encode('utf-8'))
-                                            time.sleep(5)
-                                        count = count + 1
-                                        # print ('Tweet from @%s Date: %s' % (tweet['user']['screen_name'].encode('utf-8'),tweet['created_at']))
-                                        # print (tweet['text'].encode('utf-8'), '\n', polarity_result)
+                                        cursor = select_error_string(tweet['id'])
+                                        for row in cursor:
+                                            if row[0] == 0:
+                                                if len(tweet['text']) > 200 or "https" in tweet['text']:
+                                                    twitter.retweet(id=int(tweet['id']))
+                                                    print(tweet['text'])
+                                                    sendtotelegram(TELEGRAM_TOKEN, TELEGRAM_TELEGRAM_CHAT_ID, tweet['text'])
+                                                    time.sleep(5)
+                                                else:
+                                                    tempStatus = "RT @" + tweet['user']['screen_name'] + " : " + tweet['text'] + " via #5k6mbot"
+                                                    # this code is commented because of difficult in identifying the polls. #TODO
+                                                    # twitter.update_status(status=tempStatus.encode('utf-8'))
+                                                    twitter.retweet(id=int(tweet['id']))
+                                                    sendtotelegram(TELEGRAM_TOKEN, TELEGRAM_TELEGRAM_CHAT_ID, tweet['text'])
+                                                    print(tempStatus.encode('utf-8'))
+                                                    time.sleep(5)
+                                                count = count + 1
+                                                # print ('Tweet from @%s Date: %s' % (tweet['user']['screen_name'].encode('utf-8'),tweet['created_at']))
+                                                # print (tweet['text'].encode('utf-8'), '\n', polarity_result)
                                     except TwythonError as e:
                                         if "You have already retweeted this Tweet" in str(e):
-                                            continue
+                                            insert_error_string(tweet['id'])
+                                            print("error on action", tweet['id'], e)
                                         else:
-                                            print(e)
+                                            print("error on action ", e)
                         # print ("total filtered and retweeted..." + str(count))
-        # added this to avoid "Twitter API returned a 429 (Too Many Requests), Rate limit exceeded"
-        time.sleep(10)
+                # added this to avoid "Twitter API returned a 429 (Too Many Requests), Rate limit exceeded"
+                time.sleep(10)
         # print ("end of search")
     print("sleeping for 1hr")
     time.sleep(3600)
